e1303af26301a0c6731a11cb21f6b593c470a348
Sara Plazas <s.plazas@uniandes.edu.co>
2020-10-21 19:56:00 -0500

    Implementacion

---
+++ TallerTypescript/dataCourses.ts
@@ -0,0 +1,11 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Calculo vectorial", "Alexander Berenstein", 3),
+  new Course("Pobabilidad y estadistica I", "Alex Murcia", 3),
+  new Course("Frances", "Zaide Figueredo", 3),
+  new Course("Sistemas de control gerencial", "Miguel Garcia", 3),
+  new Course("Desarrollo de SW en equipo", "Rubby Casallas", 3),
+  new Course("Seleccion voleibol playa", "Rafael Noriega", 1),
+  new Course("Principios de optimizacion", "Andres Medaglia", 3)
+]
